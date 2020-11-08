dc5e71e2cf44f5cf21292edb839848d0439699e5
Pavel Adamovich <p.adamovich@do-it.co>
2020-11-08 13:21:56 +0200

    added eslint and prettier. formatted code

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { render } from "react-dom";
+import "@src/index.css";
+import App from "@src/App";
+import reportWebVitals from "@src/reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root"),
 );
 
 // If you want to start measuring performance in your app, pass a function
